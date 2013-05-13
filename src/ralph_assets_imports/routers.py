0249ab09a905ad1570d05529899cc2a99d21c80a
Marcin Kliks <marcin.kliks@gmail.com>
2013-05-13 20:14:59 +0200

    Added routers

---
+++ src/ralph_assets_imports/routers.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from ralph.routers import BaseRouter
+
+
+class RalphAssetsImportsRouter(BaseRouter):
+    db_name = 'ralph_assets_imports'
+    app_name = 'ralph_assets_imports'
+
